ebab967d575a1691d639b3852ba3f84a0807300a
antoinevo <antoine.monarch@gmail.com>
2018-06-27 22:43:00 -0700

    Pulled Images from Database now Showing up.

--- front-end/src/components/Emotional-flash/emotional-multiple-choice.js
+++ front-end/src/components/Emotional-flash/emotional-multiple-choice.js
@@ -3,58 +3,76 @@ import Card from './card';
 import MultipleChoiceAnswers from './multiple-choice-answers';
 import { firebaseApp } from '../../firebase';
 
-// Hardcoded Pictures. TODO: Make Request to backend instead.
 
 
 export default class EmotionalMultipleChoice extends Component {
 
-
-  //Emotional Flash is the component that displays the game and handles logic.
+  // EmotionalMultipleChoice is the component that displays the game and handles logic.
   constructor(props){
     super(props);
     this.state = {
       isCorrect: null,
       correctAnswer: null,
       selectedAnswer: null,
-      imageURLs: [],
+      imageURL: null,
+      imagesAndEmotions: [],
       answers: ["angry", "happy", "sad"]
     }
   }
 
+
+  // Set Up 
   componentWillMount(){
     this.queryDatabaseForImages();
   }
 
-  selectAnswer = (answer) => {
-    this.setState({ selectedAnswer: answer });
+  componentDidUpdate(prevProps, prevState){
+    if (this.state.imagesAndEmotions !== prevState.imagesAndEmotions) {
+      this.randomizeQuestion();
+    }
   }
 
   queryDatabaseForImages = () => {
      const db = firebaseApp.firestore();
      const allImages = db.collection("imageURL").doc("emotions").collection("all");
      const allImagesArray = [];
-
      allImages.get()
       .then(snapshot => {
         snapshot.forEach(doc => {
-         allImagesArray.push(doc.data().imageURL);
-         this.setState({ imageURLs: allImagesArray });
+         let emotionImageObject = { imageURL: doc.data().imageURL, emotion: doc.data().emotion };
+         allImagesArray.push(emotionImageObject);
+         this.setState({ imagesAndEmotions: allImagesArray });
         });
       }).catch( err => {
         console.log('error' , err);
       })
   }
 
+  randomizeQuestion = () => {
+    const randomIndex = Math.floor((Math.random() * this.state.imagesAndEmotions.length));
+    const url = this.state.imagesAndEmotions[randomIndex].imageURL;
+    this.setState({ imageURL: url })
+  }
+
+
+  // Helper Functions
   checkAnswer = () => {
     this.state.correctAnswer === this.state.selectedAnswer ? this.setState({ isCorrect: true }) : this.setState({ isCorrect: false });
   }
 
+  selectAnswer = (answer) => {
+    this.setState({ selectedAnswer: answer });
+  }
+
+
+
+  // Render Function
   render() {
     return (
       <div className="emotional-flash">
         <progress style={{ width: '75%' }} className="progress is-success is-large" value={this.props.user.progress} max="100"></progress>
         <h1 className="title">Which emotion is shown in the picture?</h1>
-        {this.renderImage}
+        <img src={this.state.imageURL}></img>
         <MultipleChoiceAnswers selectedAnswer={this.state.selectedAnswer} selectAnswer={this.selectAnswer} answers={this.state.answers}/>
         <button className="button" onClick={this.checkAnswer} >Submit</button>
       </div>
