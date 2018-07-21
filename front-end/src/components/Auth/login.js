7b3bbcbad29abc242688fd167da972c27f081234
Chris Tram <chris.tram@gmail.com>
2018-07-20 22:32:28 -0700

    just format login.js

--- front-end/src/components/Auth/login.js
+++ front-end/src/components/Auth/login.js
@@ -3,65 +3,78 @@ import { NavLink } from 'react-router-dom';
 import { login, resetPassword } from './auth';
 
 export default class Login extends Component {
-	constructor(props){
-		super(props);
-	}
-	
-	logIn = (event) => {
-		event.preventDefault();
-		login(this.refs.email.value, this.refs.password.value).catch((error) => {
-			console.log(error.code);
-			console.log(error.message);
-		})
+  constructor(props) {
+    super(props);
+  }
 
-	}
+  logIn = event => {
+    event.preventDefault();
+    login(this.refs.email.value, this.refs.password.value).catch(error => {
+      console.log(error.code);
+      console.log(error.message);
+    });
+  };
 
+  renderLoginPage = () => {
+    return (
+      <section class="hero is-fullheight" id="login-hero">
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <div class="column is-4 is-offset-4">
+              <h3 class="title has-text-grey">Login</h3>
+              <p class="subitlte has-text-grey">Please login to proceed</p>
+              <div class="box" id="login-box">
+                <form onSubmit={this.logIn}>
+                  <div class="field">
+                    <div class="control">
+                      <input
+                        class="input is-medium"
+                        type="email"
+                        placeholder="Your Email"
+                        ref="email"
+                        required
+                      />
+                    </div>
+                  </div>
 
+                  <div class="field">
+                    <div class="control">
+                      <input
+                        class="input is-medium"
+                        type="password"
+                        placeholder="Your Password"
+                        ref="password"
+                        required
+                      />
+                    </div>
+                  </div>
 
- renderLoginPage = () => {
- 	return (
- 		<section class="hero is-fullheight" id="login-hero">
- 			<div class="hero-body">
- 				<div class="container has-text-centered">
- 					<div class="column is-4 is-offset-4">
- 						<h3 class="title has-text-grey">Login</h3>
- 						<p class="subitlte has-text-grey">Please login to proceed</p>
- 						<div class="box" id="login-box">
- 							<form onSubmit={this.logIn}>	
-	              <div class="field">
-	                <div class="control">
-	                  <input class="input is-medium" type="email" placeholder="Your Email" ref="email" required></input>
-	                </div>
-              	</div>
+                  <button class="button is-block is-info is-medium is-fullwidth">Login</button>
 
-	             <div class="field">
-	                <div class="control">
-	                  <input class="input is-medium" type="password" placeholder="Your Password" ref="password" required></input>
-	                </div>
-               </div>
+                  <p class="has-text-grey" id="login-footer">
+                    <NavLink exact className="nav-link-login" activeClassName="active" to="/SignUp">
+                      Sign Up
+                    </NavLink>{' '}
+                    &nbsp; &nbsp; &nbsp; &nbsp;
+                    <NavLink
+                      exact
+                      className="nav-link-login"
+                      activeClassName="active"
+                      to="/ForgotPassword"
+                    >
+                      Forgot Password
+                    </NavLink>
+                  </p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  };
 
-               <button class="button is-block is-info is-medium is-fullwidth" >Login</button>
-
-               <p class="has-text-grey" id="login-footer">
-               	 	<NavLink exact className="nav-link-login" activeClassName='active' to="/SignUp">Sign Up</NavLink> &nbsp; &nbsp; &nbsp; &nbsp;
-               	 	<NavLink exact className="nav-link-login" activeClassName='active' to="/ForgotPassword">Forgot Password</NavLink>
-               </p>
-
- 							</form>
- 						</div>
- 					</div>
- 				</div>
- 			</div>
- 		</section>
- 	);
- }
-
-	render() {
-		return (
-		  <div className="login" >
-		  	{this.renderLoginPage()}
-		  </div>
-		);
-	}
+  render() {
+    return <div className="login">{this.renderLoginPage()}</div>;
+  }
 }
-
